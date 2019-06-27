c65ed63054a9d54b7a0c8e11acf9564ff4c49a31
hanshungithub <hshanshun@163.com>
2019-06-27 10:22:51 +0800

    ioc

--- src/main/java/com/atguigu/config/MainConfig.java
+++ src/main/java/com/atguigu/config/MainConfig.java
@@ -11,12 +11,11 @@ import com.atguigu.bean.Person;
 
 //配置类==配置文件
 @Configuration  //告诉Spring这是一个配置类
-
 @ComponentScans(
 		value = {
 				@ComponentScan(value="com.atguigu.ioc",includeFilters = {
-/*						@Filter(type=FilterType.ANNOTATION,classes={Controller.class}),
-						@Filter(type=FilterType.ASSIGNABLE_TYPE,classes={BookService.class}),*/
+						//@Filter(type=FilterType.ANNOTATION,classes={Controller.class}),
+						//@Filter(type=FilterType.ASSIGNABLE_TYPE,classes={BookService.class}),
 						@Filter(type=FilterType.CUSTOM,classes={MyTypeFilter.class})
 				},useDefaultFilters = false)
 		}
