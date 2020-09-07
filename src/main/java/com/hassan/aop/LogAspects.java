eed9c7610e1f3a91c718abc801bc7dd42304e67a
hanshunshun <hanshunshun@pinming.cn>
2020-09-07 18:50:21 +0800

    fix

--- src/main/java/com/hassan/aop/LogAspects.java
+++ src/main/java/com/hassan/aop/LogAspects.java
@@ -8,10 +8,15 @@ import org.aspectj.lang.annotation.*;
 
 /**
  * 切面类
- * @author lfy
+ * @author hassan
  * 
  * @Aspect： 告诉Spring当前类是一个切面类
  *
+ * Aspect(切面):通常是一个类，里面可以定义切入点和通知
+ * JointPoint(连接点):程序执行过程中明确的点，一般是方法的调用
+ * Advice(通知):AOP在特定的切入点上执行的增强处理，有before(前置),after(后置),afterReturning(最终),afterThrowing(异常),around(环绕)
+ * Pointcut(切入点):就是带有通知的连接点，在程序中主要体现为书写切入点表达式
+ * weave(织入)：将切面应用到目标对象并导致代理对象创建的过程
  */
 @Aspect //切面类
 public class LogAspects {
